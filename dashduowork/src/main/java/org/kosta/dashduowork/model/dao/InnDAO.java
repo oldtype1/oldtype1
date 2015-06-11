41fbf98f0810d53fd5c633be2bf8017db8418811
oldtype1 <oldtype_1@naver.com>
2015-06-11 11:43:24 +0900

    test

--- dashduowork/src/main/java/org/kosta/dashduowork/model/dao/InnDAO.java
+++ dashduowork/src/main/java/org/kosta/dashduowork/model/dao/InnDAO.java
@@ -1,5 +1,16 @@
 package org.kosta.dashduowork.model.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.kosta.dashduowork.model.vo.InnVO;
+import org.kosta.dashduowork.model.vo.MemberVO;
+
+
 public interface InnDAO {
+	
+	public abstract List<InnVO> getmyinnlist(HashMap<String,String> param);
+
+	public abstract int getTotalPostingCount(MemberVO vo);
 
 }
