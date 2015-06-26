73da213ea40fd669163d368ec756c656b22a4b19
KOSTA <KOSTA@KOSTA-HP>
2015-06-26 16:34:26 +0900

    검색관련 메서드 수정 및 필요없는부분 삭제 정리

--- dashduowork/src/main/java/org/kosta/dashduowork/model/dao/InnDAOImpl.java
+++ dashduowork/src/main/java/org/kosta/dashduowork/model/dao/InnDAOImpl.java
@@ -34,35 +34,11 @@ public class InnDAOImpl implements InnDAO {
 	public List<InnVO> getmyinnlist(HashMap<String, String> param) {
 		return sqlSessionTemplate.selectList("inn.getmyinnlist",param);
 	}
-	
-	@Override //은식,동원
-	public List<InnVO> selectInnByCityAndAcceptableNo(InnVO vo) {
-		return sqlSessionTemplate.selectList("inn.selectInnByCityAndAcceptableNo", vo);
-	}
-
-	@Override //은식,동원
-	public List<InnVO> selectInnByCheckedAmenity(AmenityVO vo) {
-		return sqlSessionTemplate.selectList("inn.selectInnByCheckedAmenity", vo);
-	}
-	//plus+++++++++++++++++++++++++++++++++++++++++++
-	public List<InnVO> selectInnByCityAndAcceptableNo(SearchVO vo){
-		return sqlSessionTemplate.selectList("inn.selectInnByCityAndAcceptableNo", vo);
-	}
-	public List<InnVO> selectInnByCityAndDateAndAcceptableNo(SearchVO vo){ //은식,동원
-		return sqlSessionTemplate.selectList("inn.selectInnByCityAndDateAndAcceptableNo", vo);
-	}
 
 	@Override
 	public void register(InnVO vo){
 		System.out.println("inndao : "+vo);
 		sqlSessionTemplate.insert("inn.register", vo);
-	}
-	// 6/15일 추가내용******************
-	public List<InnVO> selectInnByCityAndDateAndAcceptableNoWithFilter(FilterVO vo){ //지역&날짜&인원+필터
-		return sqlSessionTemplate.selectList("inn.selectInnByCityAndDateAndAcceptableNoWithFilter", vo);
-	}
-	public List<InnVO> selectInnByCityAndAcceptableNoWithFilter(FilterVO vo){ //지역&인원+필터
-		return sqlSessionTemplate.selectList("inn.selectInnByCityAndAcceptableNoWithFilter", vo);
 	}
 	
 	// 은수
@@ -85,8 +61,8 @@ public class InnDAOImpl implements InnDAO {
 	}
 	//6/17일 추가(지역 자동완성)
 	@Override
-	public List<InnVO> selectInnCityListByInnCityCharacter(SearchVO vo) {
-		return sqlSessionTemplate.selectList("inn.selectInnCityListByInnCityCharacter",vo);
+	public List<InnVO> selectInnCityListByInnCityCharacter(FilterVO fvo) {
+		return sqlSessionTemplate.selectList("inn.selectInnCityListByInnCityCharacter", fvo);
 	}
 	//6/18일 추가(+가격)
 	public List<InnVO> selectInnByCityAndAcceptableNoWithPrice(FilterVO vo) {
